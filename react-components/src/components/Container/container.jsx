5a1cc1b5703b2ecf8f01ea0ced78ab71e9e83956
Temmyogunbo <Ogunbotemilola@yahoo.com>
2019-12-30 13:59:37 +0100

    add container

---
+++ react-components/src/components/Container/container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+const StyledContainer = styled.div`
+max-width: 50em;
+margin: 0 auto;
+`
+
+export const Container = React.forwardRef((props, ref) => {
+  <StyledContainer ref={ref}>{props.children}</StyledContainer>
+})
