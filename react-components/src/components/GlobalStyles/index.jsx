b5795fdfd67f270fafa5daaeeacf7fdab3be817e
Temmyogunbo <Ogunbotemilola@yahoo.com>
2019-12-30 14:00:49 +0100

    add globals

---
+++ react-components/src/components/GlobalStyles/index.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+   box-sizing: border-box;
+}
+body, html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+`;
