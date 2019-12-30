25b138011b1c6bc062702f406b28d4ce7a5f3c1f
Temmyogunbo <Ogunbotemilola@yahoo.com>
2019-12-30 14:01:30 +0100

    add utils

---
+++ react-components/src/constants/index.js
@@ -0,0 +1,20 @@
+export const SCREEN_SIZE = {
+  extraSmall: 320,
+  small: 420,
+  medium: 760,
+  large: 960,
+  extraLarge: 1200
+}
+
+export const SCREEN = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraSmall: 'xs',
+}
+
+export const MEDIA = {
+  small: 384,
+  medium: 576,
+  large: 768,
+}
---
+++ react-components/src/utils/getClassValues.js
@@ -0,0 +1,85 @@
+import { SCREEN, MEDIA } from '../constants';
+
+export const getClassValues = ({props, browserScreenSize}) => {
+  return Object.keys(props).reduce((accum, curr) => {
+    switch (true) {
+      case SCREEN.small === curr &&
+        browserScreenSize >= MEDIA.small &&
+        browserScreenSize < MEDIA.medium:
+        switch (props[curr]) {
+          case 1:
+            accum["sm-Grid--full"] = true;
+            break;
+          case 2:
+            accum["sm-Grid--1of2"] = true;
+            break;
+          case 3:
+            console.log("three");
+            accum["sm-Grid--1of3"] = true;
+            break;
+
+          default:
+            accum["sm-Grid--1of4"] = true;
+            break;
+        }
+        break;
+      case SCREEN.medium === curr &&
+        browserScreenSize >= MEDIA.medium &&
+        browserScreenSize < MEDIA.large:
+        switch (props[curr]) {
+          case 1:
+            accum["med-Grid--full"] = true;
+            break;
+          case 2:
+            accum["med-Grid--1of2"] = true;
+            break;
+          case 3:
+            accum["med-Grid--1of3"] = true;
+            break;
+
+          default:
+            accum["med-Grid--1of4"] = true;
+            break;
+        }
+        break;
+      case SCREEN.large === curr && browserScreenSize >= MEDIA.large:
+        switch (props[curr]) {
+          case 1:
+            accum["lg-Grid--full"] = true;
+            break;
+          case 2:
+            accum["lg-Grid--1of2"] = true;
+            break;
+          case 3:
+            accum["lg-Grid--1of3"] = true;
+            break;
+
+          default:
+            accum["lg-Grid--1of4"] = true;
+            break;
+        }
+        break;
+
+      case SCREEN.extraSmall === curr && browserScreenSize < MEDIA.small:
+        switch (props[curr]) {
+          case 1:
+            accum["xs-Grid--full"] = true;
+            break;
+          case 2:
+            accum["xs-Grid--1of2"] = true;
+            break;
+          case 3:
+            accum["xs-Grid--1of3"] = true;
+            break;
+
+          default:
+            accum["xs-Grid--1of4"] = true;
+            break;
+        }
+        break;
+      default:
+        break;
+    }
+    return accum;
+  }, {});
+};
